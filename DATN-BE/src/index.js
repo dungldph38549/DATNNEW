7bc4490479dd1e6aed418164cc1151f5ad57eca3
ph50176 <tuantmph50176@gmail.com>
2025-06-16 17:38:50 +0700

    banner , review BE

--- DATN-BE/src/index.js
+++ DATN-BE/src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import chat_lieu_router from './routess/chat_lieu.js';
+import banner_router from './routess/banner.js';
+import review_router from './routess/review.js';
 
 
 dotenv.config();
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use('/api/chat-lieu', chat_lieu_router);
+app.use('/api/banners', banner_router);
+app.use('/api/reviews', review_router);
 
 
 mongoose.connect("mongodb://localhost:27017/datn_su25_database")
